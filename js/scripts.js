0996abf6eb428f808601a10f023870cae7838b63
lemic00 <michtn.le@gmail.com>
2024-01-09 04:14:53 -0600

    Update scripts.js
    - added responsive mobile navigation menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hello! Welcome to my portfolio website.");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle')
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    }
+    else {
+        x.className = 'navtoggle';
+    }
+}
